392951d9c3aa679ed67579f05b345b196ab3838c
NJTYTYTY <iq252551@gmail.com>
2025-09-28 21:19:49 -0900

    now update

--- start_controller.sh
+++ start_controller.sh
@@ -3,5 +3,5 @@
 source /home/rwb/control/venv/bin/activate
 
 # Run Python script
-exec python3 /home/rwb/control/controller.py
-
+exec python3 /home/rwb/control/controller.py &
+exec python /home/rwb/control/heartbeat.py 
